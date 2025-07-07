b23f354a5578df7fa55df10dfb1d609392e10eac
penielny <penielnyinaku@gmail.com>
2025-07-07 17:18:20 +0000

    feat: Add routing and components for client and admin layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ClientComponent } from './layouts/client/client.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "/app"
+    },
+    {
+        path: "app",
+        component: ClientComponent
+    },
+    {
+        path: "admin",
+        loadComponent: () => import('./layouts/admin/admin.component').then(c => c.AdminComponent),
+        children: [
+            {
+
+            }
+        ]
+
+    },
+    {
+        path: "**",
+        component: NotFoundComponent
+    }
+];
