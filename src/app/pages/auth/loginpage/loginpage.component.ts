0e1f7b93ba36b63fd4fdbab0636a4282e7ba54bd
penielny <penielnyinaku@gmail.com>
2025-07-09 12:49:42 +0000

    feat: Enhance login page with form validation and responsive styles

--- src/app/pages/auth/loginpage/loginpage.component.ts
+++ src/app/pages/auth/loginpage/loginpage.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.scss'
 })
 export class LoginpageComponent {
+ loginForm: FormGroup;
+
+ constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+    onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 
 }
