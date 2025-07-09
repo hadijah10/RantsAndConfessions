0c2ce3e1bc9c0bfb301f9dfa18358b000ac26433
penielny <penielnyinaku@gmail.com>
2025-07-09 10:13:41 +0000

    feat: Implement authentication and client routing with new components

--- src/app/pages/auth/signuppage/signuppage.component.spec.ts
+++ src/app/pages/auth/signuppage/signuppage.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { SignuppageComponent } from './signuppage.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('SignuppageComponent', () => {
+  let component: SignuppageComponent;
+  let fixture: ComponentFixture<SignuppageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [SignuppageComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(SignuppageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
