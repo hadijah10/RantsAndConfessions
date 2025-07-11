9bf5263b7b854300a1db86dcd8c1d62b9b21b0bf
Your Name <mohamaduhadijatu@gmail.com>
2025-07-11 08:07:53 -0400

    fix: update pagination functionality with template

--- src/app/pages/client/episodes/episodes.component.ts
+++ src/app/pages/client/episodes/episodes.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import { EpisodesService } from '../../../services/api-consumer/episodes/episodes.service';
+import { PodcastEpisode } from '../../../model/episodedata.interface';
 
 @Component({
   selector: 'app-episodes',
-  imports: [],
+  imports: [MatPaginatorModule],
   templateUrl: './episodes.component.html',
   styleUrl: './episodes.component.scss'
 })
 export class EpisodesComponent {
+
+   allEpisodes: PodcastEpisode[] = [];
+  paginatedEpisodes: PodcastEpisode[] = [];
+
+   totalItems = 0;
+  pageSize = 5;
+  currentPage = 0;
+  constructor (private episodeservice: EpisodesService){
+    this.fetchEpisodes();
+  }
+
+  fetchEpisodes(){
+    this.episodeservice.getEpisodes().subscribe(data => {
+      this.allEpisodes = data.data;
+      this.totalItems = data.data.length;
+      this.updatePaginatedEpisodes();
+    });
+  }
+
+  updatePaginatedEpisodes(){
+      const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    this.paginatedEpisodes = this.allEpisodes.slice(start, end);
+  }
+
+    onPageChange(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.updatePaginatedEpisodes();
+  }
 
 }
