c548a6565191c1662ae80c7e2b9ef0bfcdd5a762
Your Name <mohamaduhadijatu@gmail.com>
2025-07-11 09:03:10 -0400

    feat: add component for episode details

---
+++ src/app/pages/client/episodedetails/episodedetails/episodedetails.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-episodedetails',
+  imports: [],
+  templateUrl: './episodedetails.component.html',
+  styleUrl: './episodedetails.component.scss'
+})
+export class EpisodedetailsComponent {
+
+}
