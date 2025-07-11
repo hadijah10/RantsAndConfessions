5db529f535b6e5165a6dd3e8b6fce448d9968cf8
Your Name <mohamaduhadijatu@gmail.com>
2025-07-11 08:20:21 -0400

    fix:update paginator service with implementation

--- src/app/services/paginatorIntl/paginator-intl.service.ts
+++ src/app/services/paginatorIntl/paginator-intl.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PaginatorIntlService {
+export class PaginatorIntlService extends MatPaginatorIntl{
+  //override the function which displays the default content
+  override getRangeLabel = (page:number, pageSize: number, length: number) => {
+    return `Page ${page + 1} of ${Math.ceil(length/pageSize)}`
+  }
 
-  constructor() { }
 }
