ca331a14c28d61e89d5ba68f2bb51a21fcf7b3bd
Your Name <mohamaduhadijatu@gmail.com>
2025-07-11 00:07:21 -0400

    feat: add confessions page as well as update navbar links

--- src/app/pages/client/confessionpage/confessionpage.component.ts
+++ src/app/pages/client/confessionpage/confessionpage.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder,Validators,FormGroup,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-confessionpage',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './confessionpage.component.html',
   styleUrl: './confessionpage.component.scss'
 })
 export class ConfessionpageComponent {
+  confessionsForm!: FormGroup;
+  categories = ['Funny', 'Suggestion', 'Sad','Lesson', 'Other'];
 
+  constructor(private fb: FormBuilder) {
+      this.confessionsForm = this.fb.group({
+      message: ['', [Validators.required, Validators.minLength(5)]],
+      category: ['', Validators.required],
+      emotion: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+  
+}
+
+  onSubmit(){
+
+  }
 }
