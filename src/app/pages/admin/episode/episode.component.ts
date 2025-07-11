538df4f8285160a9a988bf742385a2707f6b4e33
penielny <penielnyinaku@gmail.com>
2025-07-11 17:44:56 +0000

    feat: implement confession and episode state management with services and components

--- src/app/pages/admin/episode/episode.component.ts
+++ src/app/pages/admin/episode/episode.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IAddEpisodeToPlaylistApiResponse, IEpisode } from '../../../model/playlistdata.interface';
+import { EpisodeStateService } from '../../../services/app-state/episode-state.service';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-episode',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './episode.component.html',
   styleUrl: './episode.component.scss'
 })
-export class EpisodeComponent {
-  episodes: IEpisode[] = []
+export class EpisodeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  episodes: IEpisode[] = []
+  subscription: Subscription = new Subscription()
+
+  constructor(private episodeState: EpisodeStateService) {
+    this.episodeState.getEpisodes();
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.episodeState.episodeState$.subscribe(value => {
+      this.episodes = value?.data || []
+    })
+  }
 
   toggleCreate() {
 
+  }
+
+  onDelete(ep: IEpisode) {
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 
 }
--- src/app/services/api-consumer/confessions/confession.service.ts
+++ src/app/services/api-consumer/confessions/confession.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IGetConfessionData, IPostConfessionData, IPostConfessionDataResponse } from '../../../model/confessiondata.interface';
+import { IConfessionResponse, Ifilter, IGetConfessionData, IPostConfessionData, IPostConfessionDataResponse } from '../../../model/confessiondata.interface';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorService } from '../../error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +10,19 @@ import { IGetConfessionData, IPostConfessionData, IPostConfessionDataResponse } 
 export class ConfessionService {
   private url = `https://api.rantsnconfess.com/v1/confessions`
 
-  constructor(private http: HttpClient) { }
-  getConfessions(){
-    //only possible with authentication
-   return this.http.get<IGetConfessionData[]>(this.url).pipe()
+  constructor(private http: HttpClient, private errorService: ErrorService) { }
+  
+  getConfessions(status: Ifilter = ""):Observable<IConfessionResponse> {
+    return this.http.get<IConfessionResponse>(status? `${this.url}?status=${status}&page=2`: this.url).pipe(
+      catchError(error => {
+        this.errorService.handleError(error)
+        return of(error)
+      })
+    )
   }
 
-  postConfessions(data: IPostConfessionData){
-    return this.http.post<IPostConfessionDataResponse>(this.url,data).pipe()
+  postConfessions(data: IPostConfessionData) {
+    return this.http.post<IPostConfessionDataResponse>(this.url, data).pipe()
   }
 
 
