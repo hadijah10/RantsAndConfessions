fab5cfc10cad4168b2e57354728d3e56b2aa6473
Your Name <mohamaduhadijatu@gmail.com>
2025-07-13 16:58:11 -0400

    feat: generate component for theme switching

---
+++ src/app/components/themeswitcher/themeswitcher.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-themeswitcher',
+  imports: [],
+  templateUrl: './themeswitcher.component.html',
+  styleUrl: './themeswitcher.component.scss'
+})
+export class ThemeswitcherComponent {
+
+}
