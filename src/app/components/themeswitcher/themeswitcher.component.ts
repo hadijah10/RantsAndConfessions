35fd5d9a4e4ab69788fc506c2374490c3b5abe7d
Your Name <mohamaduhadijatu@gmail.com>
2025-07-13 17:24:37 -0400

    fix: add theme switching buttons and its styling

--- src/app/components/themeswitcher/themeswitcher.component.ts
+++ src/app/components/themeswitcher/themeswitcher.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-themeswitcher',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './themeswitcher.component.html',
   styleUrl: './themeswitcher.component.scss'
 })
-export class ThemeswitcherComponent {
+export class ThemeswitcherComponent implements OnInit{
+  theme:string =  JSON.parse(localStorage.getItem('theme' )?? 'light')
+  constructor(private renderer:Renderer2){
+       const localstoragedata = JSON.parse(localStorage.getItem('theme') ?? 'null');
+    if(localstoragedata == null){
+      localStorage.setItem('theme','light')
+    }
+  }
 
+toggletheme(){
+  if(this.theme == 'light'){
+    this.renderer.removeClass(document.body,'dark-theme')
+  }
+  else{
+    this.renderer.addClass(document.body,'dark-theme')
+  }
+}
+  ngOnInit(){
+ 
+  }
 }
