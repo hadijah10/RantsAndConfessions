3373370b872a90077e5083780255c225ea0bef03
Your Name <mohamaduhadijatu@gmail.com>
2025-07-13 16:39:22 -0400

    fix:update css styling for the team members on home page

--- src/app/pages/client/homepage/homepage.component.ts
+++ src/app/pages/client/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { PlaylistService } from '../../../services/api-consumer/playlist/playlist.service';
 import { IPlaylistListApiResponse } from '../../../model/playlistdata.interface';
 import { Observable } from 'rxjs';
@@ -8,6 +8,8 @@ import { IPodcastApiResponse } from '../../../model/episodedata.interface';
 import { EpisodecardComponent } from '../../../components/episodecard/episodecard.component';
 import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../../components/button/button.component';
+import { TeamMembersService } from '../../../services/api-consumer/team-members/team-members.service';
+import { ITeamMember } from '../../../model/teammembersdata.interface';
 
 @Component({
   selector: 'app-homepage',
@@ -18,11 +20,22 @@ import { ButtonComponent } from '../../../components/button/button.component';
 export class HomepageComponent {
   featplaylist$!:Observable<IPlaylistListApiResponse>
   episodesdata$!:Observable<IPodcastApiResponse>
-  // teamMembers$!:
+  teamMembers!:ITeamMember[]
+  isLoading= true
+  isError = signal<string | null>(null)
 
-  constructor(private playlist:PlaylistService,private episodes:EpisodesService){
+  constructor(private playlist:PlaylistService,private episodes:EpisodesService,private teammembersService: TeamMembersService){
     this.featplaylist$ = this.playlist.getPlaylist()
     this.episodesdata$ =  this.episodes.getEpisodes(1)
+    this.teammembersService.getTeamMembers().subscribe({
+      next:(data) => {
+        this.isLoading = false
+        this.teamMembers = data.data},
+      error:(error) => {
+        this.isLoading = false
+        this.isError.set('An error occurred')
+      }
+    })
   }
 
 
